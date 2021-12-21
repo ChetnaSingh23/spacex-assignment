db614035a10e195a33d50cb2540ea187a03cb4e7
Chetna Singh <chetnasingh023@gmail.com>
2021-12-21 16:25:11 +0530

    Adds filters and logic to display past launches of 2018

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // Please run your solution from this file
 
-console.log("Hello from %csrc/index.js", "font-weight:bold");
+import { fetchPastLaunches } from "./solution";
+
+// and check the output in the browser.
+window.onload = function() {
+  fetchPastLaunches(); // fetch past launches and display them in the browser
+}
--- src/solution.js
+++ src/solution.js
@@ -1,8 +1,62 @@
 // Please implement your solution in this file
 
-const noop = () => {};
+export const fetchPastLaunches = () => {
+  const url = 'https://api.spacexdata.com/v3/launches/past';
+
+  fetch(url, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const filteredData = prepareData(data) //Prepare data
+      renderData(filteredData) //Render data
+})
+}
+
+const prepareData = (data) => {  
+  const filterParam = { launch_year: '2018', customerName : 'nasa' }; //filterParam as per requirement
+
+  try {
+      return data.filter(item => {
+          const payloads = item.rocket && item.rocket.second_stage && item.rocket.second_stage.payloads
+        
+          return item.launch_year === filterParam.launch_year && payloads
+          .find(payload => payload.customers
+            .filter(customer => customer.toLowerCase() // toLowerCase() is used to ignore case sensitivity
+            .includes(filterParam.customerName)).length > 0) // includes() is used to check if the customerName is present in the payload.customers array
+        })
+        .map(item => {
+          return {
+                flight_number: item.flight_number,
+                mission_name: item.mission_name,
+                payloads_count: item.rocket.second_stage.payloads.length, // payloads.length is used to get the count of payloads
+            }
+        })
+        .sort((a, b) => b.flight_number - a.flight_number) //Sort by flight number
+        .sort((a, b) =>  b.payloads_count - a.payloads_count) //Sort by payloads count
+    
+  } catch (error) {
+    console.error(error) //Log error
+  }
+}
+
+const renderData = (data) => {
+  const node = document.getElementById('out');
+
+  try {
+      const jsonText = JSON.stringify(data,null, 2)
+      node.innerHTML = jsonText;
+    
+  } catch (error) {
+    node.innerHTML = error;
+  }
+}
 
 module.exports = {
-  prepareData: noop,
-  renderData: noop
+  prepareData,
+  renderData,
+  fetchPastLaunches,
 };
